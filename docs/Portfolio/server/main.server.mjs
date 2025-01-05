f14f643a37c23cf8e970d8045e92c401d5c98682
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-01-06 01:18:41 +0530

    deploy issues

---
+++ docs/Portfolio/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ZTKX2W5J.mjs";import"./chunk-YG6MMP7F.mjs";import"./chunk-XJDH2I5G.mjs";import"./chunk-XVZWWQNM.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
