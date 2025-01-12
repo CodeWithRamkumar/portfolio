d955e11b4fe66598206f8fa1a40b673ff892ea66
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-01-12 19:40:43 +0530

    url change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 export const routes: Routes = [
     {
-        path: "home",
+        path: "portfolio/home",
         loadComponent: () => import('./main-layout/main-layout.component').then((x) => x.MainLayoutComponent),
         // component:MainLayoutComponent
     },
     {
         path: "**",
-        redirectTo:"home"
+        redirectTo:"portfolio/home"
     }
 ];
