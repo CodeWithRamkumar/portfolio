d9e7263f9288311e047caa12d010ae8d2a7df6a1
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-01-12 20:50:39 +0530

    url change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: "",
+        path: "home",
         loadComponent: () => import('./main-layout/main-layout.component').then((x) => x.MainLayoutComponent),
         // component:MainLayoutComponent
     },
