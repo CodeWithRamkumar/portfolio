4623c4b35132c2d68e7f5764d33e76a9a6e4817d
CodeWithRamkumar <ramkumar03251999@gmail.com>
2025-01-12 20:40:19 +0530

    url change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
